4c49af06a21f6d3272b28742d1fb230f9eab9db8
Samuel Loureiro <samueloureiro@uol.com.br>
2023-10-17 16:53:08 -0300

    Criação das funções do caixa eletronico e arrumando a estética da saída

--- Caixa.dart
+++ Caixa.dart
@@ -2,61 +2,148 @@ import 'dart:io';
 
 import './Cliente.dart';
 
-//Cliente depositar(double val,Cliente cli){
-  
-//}
+void sacar(double val,Cliente cli) {
+    if (val <=cli.saldo ) {
+      cli.saldo -= val;
+      print("Seu saldo atual é R\$${cli.saldo}");
+    } else {
+      print("Saldo insuficiente");
+    }
+  }
+void depositar(double val,Cliente cli){
+  cli.saldo+=val;
+  print("Seu saldo atual é R\$${cli.saldo}");
+}
+
+void consultar(Cliente cli){
+  print("Seu saldo atual é R\$${cli.saldo}");
+
+}
+
+void transferir(double val,Cliente cli1,Cliente cli2){
+  if(val<=cli1.saldo){
+    cli1.saldo-=val;
+    cli2.saldo+=val;
+    print("Transferência realizada com sucesso!");
+  }else{
+    print("Saldo insuficiente");
+  }
+}
 
 
-
-//List<Cliente> clientes;
-
+List<Cliente> clientes = [
+  Cliente(nome:'João', cpf:'123.456.789-00',saldo: 1000.0),
+  Cliente(nome:'Maria', cpf:'987.654.321-00',saldo: 2000.0),
+  Cliente(nome:'Pedro', cpf:'111.222.333-44',saldo:3000.0),
+];
 void main(){
+  Cliente cli=new Cliente(nome:'',cpf:'',saldo:0);
+  double val=0;
   int op=0;
-  print("Bem vindo ao banco Asimov Jr.");
+  while(true){print("Bem vindo ao banco Asimov Jr.");
   print(" ");
-  print("Vocẽ já é um cliente nosso? (s/n)");
+  stdout.write("Vocẽ já é um cliente nosso? (s/n): ");
   String? res=stdin.readLineSync();
   if(res=='s'){
 
   
-  print("Digite seu cpf")
+  stdout.write("Digite seu cpf: ");
+  String? id=stdin.readLineSync();
+  for(int i=0;i<clientes.length;i++){
+    if(clientes[i].cpf==id){
+      cli=clientes[i];
+    }
+  }
+  print(" ");
+  print("Seja bem vindo ${cli.nome}!");
+  print(" ");
   print("1 - Sacar");
   print("2 - Depositar");
   print("3 - Transferir");
   print("4 - Consultar");
   print("5 - Sair");
   String? entrada=stdin.readLineSync();
-  if(entrada!=null){
-  op=int.parse(entrada);
-  }
+  
+  op=int.parse(entrada!);
+  
 
   while(op!=5){
     
-    /*switch(op){
+    switch(op){
       case 1:
-        //fn sacar
+        print("-------------------------- ");
+        stdout.write("Quantos reais você deseja sacar: ");
+        String? entrada=stdin.readLineSync();
+        
+        val=double.parse(entrada!);
+
+        print(" ");
+        sacar(val,cli);
+        print("-------------------------- ");
+        break;
       case 2:
-        //fn depositar
+        print("-------------------------- ");
+        stdout.write("Quantos reais você deseja depositar: ");
+        String? entrada=stdin.readLineSync();
+       
+        val=double.parse(entrada!);
+
+        print(" ");
+        depositar(val,cli);
+        print("-------------------------- ");
+        break;
       case 3:
-        //fn transferir
+        stdout.write("Digite o cpf do destinatário: ");
+        String? id=stdin.readLineSync();
+        
       case 4:
-        //fn consultar
-      case 5
-        //fn criar conta
-     
-    }*/
+        print("-------------------------- ");
+        print(" ");
+        consultar(cli);
+        print(" ");
+        print("-------------------------- ");
+      default:
+        print("-------------------------- ");
+        print(" ");
+        print("Opção inválida");
+        print(" ");
+        print("-------------------------- ");
+    }
      print("1 - Sacar");
       print("2 - Depositar");
       print("3 - Transferir");
       print("4 - Consultar");
       print("5 - Sair");
       String? entrada=stdin.readLineSync();
-      if(entrada!=null){
-      op=int.parse(entrada);
-    }
+     
+      op=int.parse(entrada!);
+    
   }
 }else{
-  print("Gostaria de criar uma conta? (s/n)");
-
+  stdout.write("Gostaria de criar uma conta? (s/n): ");
+  String? res=stdin.readLineSync();
+  if(res=='s'){
+    stdout.write("Digite seu nome:");
+    String? n=stdin.readLineSync();
+    String nome=n!;
+    stdout.write("Digite seu cpf");
+    String? c=stdin.readLineSync();
+    String cpf=c!;
+    
+    clientes.add(Cliente(nome:nome,cpf:cpf,saldo:0.0));
+    print("-------------------------- ");
+    print(" ");
+    print("Conta criada com sucesso!");
+    print(" ");
+    print("-------------------------- ");
+  }else{
+    print("-------------------------- ");
+    print(" ");
+    print("Obrigado por usar nossos serviços!");
+    print(" ");
+        print("-------------------------- ");
+    break;
+  }
 }
+  }
 }
