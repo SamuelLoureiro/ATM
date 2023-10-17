036d6e327adadbbee1ca3664f5a5703c1ee05e24
Samuel Loureiro <samueloureiro@uol.com.br>
2023-10-17 18:15:45 -0300

    Ultimas mudanças, finalizando a estética e a função transferir

--- Caixa.dart
+++ Caixa.dart
@@ -5,18 +5,30 @@ import './Cliente.dart';
 void sacar(double val,Cliente cli) {
     if (val <=cli.saldo ) {
       cli.saldo -= val;
-      print("Seu saldo atual é R\$${cli.saldo}");
+      print("-------------------------- ");
+    print(" ");
+  print("Seu saldo atual é R\$${cli.saldo}");
+  print(" ");
+    print("-------------------------- ");
     } else {
       print("Saldo insuficiente");
     }
   }
 void depositar(double val,Cliente cli){
   cli.saldo+=val;
+  print("-------------------------- ");
+    print(" ");
   print("Seu saldo atual é R\$${cli.saldo}");
+  print(" ");
+    print("-------------------------- ");
 }
 
 void consultar(Cliente cli){
+  print("-------------------------- ");
+    print(" ");
   print("Seu saldo atual é R\$${cli.saldo}");
+  print(" ");
+    print("-------------------------- ");
 
 }
 
@@ -24,44 +36,87 @@ void transferir(double val,Cliente cli1,Cliente cli2){
   if(val<=cli1.saldo){
     cli1.saldo-=val;
     cli2.saldo+=val;
+    print("-------------------------- ");
+    print(" ");
     print("Transferência realizada com sucesso!");
+    print(" ");
+    print("-------------------------- ");
+    print("-------------------------- ");
+    print(" ");
+    print("Seu saldo atual é R\$${cli1.saldo}");
+    print(" ");
+    print("-------------------------- ");
   }else{
+    print("-------------------------- ");
+    print(" ");
     print("Saldo insuficiente");
+     print(" ");
+    print("-------------------------- ");
   }
 }
 
 
 List<Cliente> clientes = [
-  Cliente(nome:'João', cpf:'123.456.789-00',saldo: 1000.0),
-  Cliente(nome:'Maria', cpf:'987.654.321-00',saldo: 2000.0),
-  Cliente(nome:'Pedro', cpf:'111.222.333-44',saldo:3000.0),
+  Cliente(nome:'João', cpf:'1',saldo: 1000.0),
+  Cliente(nome:'Maria', cpf:'2',saldo: 2000.0),
+  Cliente(nome:'Pedro', cpf:'3',saldo:3000.0),
 ];
 void main(){
   Cliente cli=new Cliente(nome:'',cpf:'',saldo:0);
   double val=0;
   int op=0;
-  while(true){print("Bem vindo ao banco Asimov Jr.");
+  while(true){
+     print("-------------------------- ");
+    print(" ");
+    print("Bem vindo ao banco Asimov Jr.");
   print(" ");
+    print("-------------------------- ");
+    print("-------------------------- ");
+    print(" ");
   stdout.write("Vocẽ já é um cliente nosso? (s/n): ");
+  
   String? res=stdin.readLineSync();
   if(res=='s'){
 
   
-  stdout.write("Digite seu cpf: ");
-  String? id=stdin.readLineSync();
-  for(int i=0;i<clientes.length;i++){
-    if(clientes[i].cpf==id){
-      cli=clientes[i];
+  while(true){
+    print("-------------------------- ");
+    print(" ");
+      stdout.write("Digite seu cpf: ");
+        
+      String? id=stdin.readLineSync();
+      for(int i=0;i<clientes.length;i++){
+        if(clientes[i].cpf==id){
+          cli=clientes[i];
+          break;
+        }else if(i==clientes.length-1){
+          print("-------------------------- ");
+    print(" ");
+          print("CPF não encontrado");
+            print(" ");
+    print("-------------------------- ");
+        }
+    }
+    if(cli.cpf!=''){
+      break;
     }
   }
-  print(" ");
+  print("-------------------------- ");
+    print(" ");
   print("Seja bem vindo ${cli.nome}!");
-  print(" ");
+   print(" ");
+    print("-------------------------- ");
+  
   print("1 - Sacar");
+  print("-------------------------- ");
   print("2 - Depositar");
+  print("-------------------------- ");
   print("3 - Transferir");
+  print("-------------------------- ");
   print("4 - Consultar");
+  print("-------------------------- ");
   print("5 - Sair");
+  print("-------------------------- ");
   String? entrada=stdin.readLineSync();
   
   op=int.parse(entrada!);
@@ -71,30 +126,66 @@ void main(){
     
     switch(op){
       case 1:
-        print("-------------------------- ");
+       print("-------------------------- ");
+    print(" ");
         stdout.write("Quantos reais você deseja sacar: ");
+       
         String? entrada=stdin.readLineSync();
         
         val=double.parse(entrada!);
 
         print(" ");
         sacar(val,cli);
-        print("-------------------------- ");
+         print(" ");
+    
         break;
       case 2:
         print("-------------------------- ");
+    print(" ");
         stdout.write("Quantos reais você deseja depositar: ");
+         print(" ");
+    print("-------------------------- ");
         String? entrada=stdin.readLineSync();
        
         val=double.parse(entrada!);
 
         print(" ");
         depositar(val,cli);
-        print("-------------------------- ");
+         print(" ");
+    
         break;
       case 3:
-        stdout.write("Digite o cpf do destinatário: ");
-        String? id=stdin.readLineSync();
+        Cliente cli2=new Cliente(nome:'',cpf:'',saldo:0);
+        while(true){
+          print("-------------------------- ");
+    print(" ");
+          stdout.write("Digite o cpf do destinatário: ");
+           
+          String? id=stdin.readLineSync();
+          for(int i=0;i<clientes.length;i++){
+            if(clientes[i].cpf==id){
+              cli2=clientes[i];
+              break;
+            }else if(i==clientes.length-1){
+              print("CPF não encontrado");
+               print(" ");
+    print("-------------------------- ");
+            }
+        }
+        if(cli2.cpf!=''){
+          break;
+        }
+        
+        } 
+        print("-------------------------- ");
+        print(" ");
+        stdout.write("Digite o valor que deseja transferir: ");
+        
+        String? entrada=stdin.readLineSync();
+        val=double.parse(entrada!);
+        print(" ");
+        transferir(val, cli, cli2);
+        print(" ");
         
       case 4:
         print("-------------------------- ");
@@ -109,24 +200,39 @@ void main(){
         print(" ");
         print("-------------------------- ");
     }
+    print(" ");
      print("1 - Sacar");
+     print("-------------------------- ");
       print("2 - Depositar");
+      print("-------------------------- ");
       print("3 - Transferir");
+      print("-------------------------- ");
       print("4 - Consultar");
+      print("-------------------------- ");
       print("5 - Sair");
+      print("-------------------------- ");
       String? entrada=stdin.readLineSync();
      
       op=int.parse(entrada!);
     
   }
 }else{
+  print("-------------------------- ");
+        print(" ");
   stdout.write("Gostaria de criar uma conta? (s/n): ");
+   
   String? res=stdin.readLineSync();
   if(res=='s'){
-    stdout.write("Digite seu nome:");
+    print("-------------------------- ");
+        print(" ");
+    stdout.write("Digite seu nome: ");
+     
     String? n=stdin.readLineSync();
     String nome=n!;
-    stdout.write("Digite seu cpf");
+    print("-------------------------- ");
+        print(" ");
+    stdout.write("Digite seu cpf: ");
+     
     String? c=stdin.readLineSync();
     String cpf=c!;
     
@@ -142,7 +248,6 @@ void main(){
     print("Obrigado por usar nossos serviços!");
     print(" ");
         print("-------------------------- ");
-    break;
   }
 }
   }
